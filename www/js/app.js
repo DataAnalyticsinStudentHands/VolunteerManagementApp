af2b8abe07479bbf7696af3080a2d6c6bac0ee53
CarlSteven <carl_steven@live.com>
2014-10-10 11:19:52 -0500

    Fixed Hours Submit, Moderation View.

--- www/js/app.js
+++ www/js/app.js
@@ -173,7 +173,7 @@ config(function($stateProvider, $urlRouterProvider, $compileProvider, Restangula
 run(['Restangular', '$rootScope', 'Auth', '$q', '$state', 'vmaUserService', 'ngNotify', function(Restangular, $rootScope, Auth, $q, $state, vmaUserService, ngNotify) {
 //    Restangular.setBaseUrl("http://localhost:8080/VolunteerApp/");            //THE LOCAL HOST
 //    Restangular.setBaseUrl("http://172.27.219.120:8080/VolunteerApp/");       //THE MAC AT CARL'S DESK
-//    Restangular.setBaseUrl("http://172.25.80.82:8080/VolunteerApp/");         //CARL'S LAPTOP
+//    Restangular.setBaseUrl("http://172.27.219.241:8080/VolunteerApp/");         //CARL'S LAPTOP
     Restangular.setBaseUrl("http://www.housuggest.org:8888/VolunteerApp/");     //HOUSUGGEST FOR VMA CORE
 
     //TO ACCESS RESTANGULAR IN CONTROLLERS WITHOUT INJECTION
